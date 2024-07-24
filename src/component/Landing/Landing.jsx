3f5afeaa3a4050c67de49f82df882487d27273c2
Ravi Gautam <rvgautam59@gmail.com>
2024-07-24 19:54:42 +0530

    add random names

--- src/component/Landing/Landing.jsx
+++ src/component/Landing/Landing.jsx
@@ -22,6 +22,7 @@ export default function Landing() {
   const names = [
     "Shivanshi just registered...",
     "Ravi just registered...",
+    "Shishir just registered...",
     "Khushi just registered...",
     "Bharat just registered...",
     "Anant just registered...",
@@ -82,7 +83,11 @@ export default function Landing() {
               <DialogTrigger>
                 <div className="register-button self-center">
                   REGISTER NOW
-                  <svg fill="currentColor" viewBox="0 0 24 24" className="register-icon">
+                  <svg
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                    className="register-icon"
+                  >
                     <path
                       clipRule="evenodd"
                       d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z"
